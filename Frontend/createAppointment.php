a65418c2798384388e2b78265c65154403e0f2d0
Aaron Renshaw <doublearenshaw@gmail.com>
2024-04-06 17:26:53 -0400

    Added logic to retrieve the technician first and last name

--- Frontend/createAppointment.php
+++ Frontend/createAppointment.php
@@ -71,10 +71,35 @@
             $randomTechnicianID = $technicianIDs[array_rand($technicianIDs)];
         }
 
+        // Creating the query to retrieve the technician's first name
+        $technicianFirstNameQuery = "SELECT firstName FROM TECHNICIANS WHERE technicianID = '$randomTechnicianID'";
+        // Running the query and storing the result as a mysqli object
+        $technicianFirstNameResult = mysqli_query($conn, $technicianFirstNameQuery);
         
+        // Retrieving the first name from the row
+        if ($technicianFirstNameResult) {
+            $firstNameRow = mysqli_fetch_assoc($technicianFirstNameResult);
+            $technicianFirstName = $firstNameRow['firstName'];
+        }
+        else {
+            $technicianFirstName = "Unknown";
+        }
+
+        // Doing the same for techncian's last name
+        $technicianLastNameQuery = "SELECT lastName FROM TECHNICIANS WHERE technicianID = '$randomTechnicianID'";
+        $technicianLastNameResult = mysqli_query($conn, $technicianLastNameQuery);
+        if ($technicianFirstNameResult) {
+            $lastNameRow = mysqli_fetch_assoc($technicianLastNameResult);
+            $technicianLastName = $lastNameRow['lastName'];
+        }
+        else {
+            $technicianLastName = "Unknown";
+        }
+
+
         // Generates a random quote for the appliance repair cost
         $randomQuote = rand(100, 300);
-
+    
         
         // Creating the insert statement for the APPOINTMENTS table
         $appointmentsInsert = "INSERT INTO APPOINTMENTS (applianceID, technicianID, customerID, appointmentDateTime, reason, quote)
@@ -88,3 +113,33 @@
         mysqli_close($conn);
     }
 ?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Appointment confirmed!</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <!--A top navigation bar to jump to other pages-->
+        <nav class="navbar">
+            <a style="text-decoration:none" href="lookup.html">EMPLOYEE PORTAL</a>
+            <a style="text-decoration:none" href="index.html">HOME</a>
+            <a style="text-decoration:none" href="registration.html">CUSTOMER PORTAL</a>
+        </nav>
+
+        <div class="header">
+                <h1>Your appointment has been confirmed!</h1>
+        </div>
+        <div class="appointmentConfirmation">
+            <p><strong>Customer:</strong> <?php echo $firstName . " " . $lastName; ?></p>
+            <p><strong>Email:</strong> <?php echo $email; ?></p>
+            <p><strong>Phone:</strong> <?php echo $phone; ?></p>
+            <p><strong>Appliance:</strong> <?php echo $appName; ?></p>
+            <p><strong>Appliance Type:</strong> <?php echo $appType; ?></p>
+            <p><strong>Technician:</strong> <?php echo $technicianFirstName . " " . $technicianLastName; ?></p>
+            <p><strong>Date & Time:</strong> <?php echo $appointmentDateTime; ?></p>
+            <p><strong>Reason:</strong> <?php echo $reason; ?></p>
+            <p><strong>Quote:</strong> <?php echo $randomQuote; ?></p>
+        </div>
